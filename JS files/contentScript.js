b88a3f7b9bc4acd21d28a8bcbfe035a6a296ab87
Brian-Masse <50341047+Brian-Masse@users.noreply.github.com>
2021-12-08 14:13:05 -0500

    Update contentScript.js

--- JS files/contentScript.js
+++ JS files/contentScript.js
@@ -22,6 +22,14 @@ const entries= [
     "_PYTHON: PW.py",
     "python",
     '\nimport matplotlib as plt\nimport seaborn as sns\n\ndark_purple = (190 / 255, 212 / 255, 255 / 255)\nmid_purple = (201 / 255, 193 / 255, 245 / 255)\ntan = (252 / 255, 209 / 255, 175 / 255)\n\nplt.rcParams.update({"font.size": 5})\npurpleCmap = sns.cubehelix_palette(as_cmap=True)\nbeachCMap = plt.colors.LinearSegmentedColormap.from_list(\n    "", [dark_purple, mid_purple, tan])\n\ndeaths = sns.load_dataset("AnimalDeaths")\npercentOfGender = deaths.pivot_table(\n    "percent", index="Animal", columns="Gender")\npercentOfAnimalKills = deaths.pivot_table(\n    "percent2", index="Animal", columns="Gender")\n\n\n# pG = sns.heatmap(percentOfGender, vmin=0, vmax=50, cmap=purpleCmap, annot=True).set(\n#     title="Deaths by an animal, as a percent of the total deaths of a gender")\npAK = sns.heatmap(percentOfAnimalKills, vmin=0, vmax=100, cmap=purpleCmap, annot=True).set(\n    title="Deaths of a gender by an animal, as a percent of the total kills by that animal")\n\nplt.pyplot.show()\n\n')
+
+    ,new ProjectEntry("PART TO WHOLE VIS",
+    "12 / 5 / 2021",
+    "This is a description of this project",
+    ["Images/12-5-21/graph1.png", "Images/12-5-21/graph2.png"],
+    "_PYTHON: PW.py",
+    "python",
+    '\nimport matplotlib as plt\nimport seaborn as sns\n\ndark_purple = (190 / 255, 212 / 255, 255 / 255)\nmid_purple = (201 / 255, 193 / 255, 245 / 255)\ntan = (252 / 255, 209 / 255, 175 / 255)\n\nplt.rcParams.update({"font.size": 5})\npurpleCmap = sns.cubehelix_palette(as_cmap=True)\nbeachCMap = plt.colors.LinearSegmentedColormap.from_list(\n    "", [dark_purple, mid_purple, tan])\n\ndeaths = sns.load_dataset("AnimalDeaths")\npercentOfGender = deaths.pivot_table(\n    "percent", index="Animal", columns="Gender")\npercentOfAnimalKills = deaths.pivot_table(\n    "percent2", index="Animal", columns="Gender")\n\n\n# pG = sns.heatmap(percentOfGender, vmin=0, vmax=50, cmap=purpleCmap, annot=True).set(\n#     title="Deaths by an animal, as a percent of the total deaths of a gender")\npAK = sns.heatmap(percentOfAnimalKills, vmin=0, vmax=100, cmap=purpleCmap, annot=True).set(\n    title="Deaths of a gender by an animal, as a percent of the total kills by that animal")\n\nplt.pyplot.show()\n\n')
 ]
 
 const HStack = document.createElement("div")
